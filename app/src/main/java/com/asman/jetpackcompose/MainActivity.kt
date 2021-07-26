cbab81ddcc7f47acb88708834ba06f84a5d749ed
Asman Mirza <asman@otmalse.com>
2021-07-26 22:24:27 +0530

    Article View in MainActivity

--- app/src/main/java/com/asman/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/asman/jetpackcompose/MainActivity.kt
@@ -3,11 +3,20 @@ package com.asman.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.asman.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,21 +24,40 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    mainView()
                 }
             }
         }
     }
 }
 
-@Composable fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true) @Composable fun DefaultPreview() {
     JetpackComposeTheme {
-        Greeting("Android")
+        mainView()
+    }
+}
+
+@Composable fun mainView() {
+
+    Column(Modifier.fillMaxWidth()) {
+        ArticleStyle()
+    }
+}
+
+@Composable fun ArticleStyle() {
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column(modifier = Modifier.padding(16.dp)) {
+            Image(painter = painterResource(R.drawable.ic_launcher_background), contentDescription = null, modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(4.dp)), contentScale = ContentScale.Crop)
+            Spacer(Modifier.height(16.dp))
+
+            Text("A day wandering through the sandhills " + "in Shark Fin Cove, and a few of the " + "sights I saw", style = typography.h6, maxLines = 2, overflow = TextOverflow.Ellipsis)
+            Text("Davenport, California", style = typography.body2)
+            Text("December 2018", style = typography.body2)
+        }
     }
 }
